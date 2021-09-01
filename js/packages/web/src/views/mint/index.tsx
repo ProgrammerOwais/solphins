7dfa7c40b2e25375cda609cef0ef53c6f5195c51
Jason Less <jaless1997@gmail.com>
2021-09-01 00:48:59 -0700

    first mint page commit

--- js/packages/web/src/views/mint/index.tsx
+++ js/packages/web/src/views/mint/index.tsx
@@ -1,27 +1,80 @@
 import Head from 'next/head'
 import 'tailwindcss/tailwind.css'
+import { useState, useEffect } from 'react';
 
 import { Layout } from 'antd';
 import React from 'react';
 
 export const MintView = () => {
+  // FOR WALLET
+  const [signedIn, setSignedIn] = useState(false)
+
+  const [walletAddress, setWalletAddress] = useState(null)
+
+  // FOR MINTING
+  const [how_many_whales, set_how_many_whales] = useState(1)
+
+  const [whaleContract, setWhaleContract] = useState(null)
+
+  // INFO FROM SMART Contract
+
+  const [totalSupply, setTotalSupply] = useState(0)
+
+  const [saleStarted, setSaleStarted] = useState(false)
+
+  const [whalePrice, setWhalePrice] = useState(0)
+  
+  async function callContractData(wallet) {
+    // let balance = await web3.eth.getBalance(wallet);
+    // setWalletBalance(balance)
+    const bananaContract = new window.web3.eth.Contract(ABI, ADDRESS)
+    setBananaContract(bananaContract)
+
+    const salebool = await bananaContract.methods.saleIsActive().call() 
+    // console.log("saleisActive" , salebool)
+    setSaleStarted(salebool)
+
+    const totalSupply = await bananaContract.methods.totalSupply().call() 
+    setTotalSupply(totalSupply)
+
+    const bananaPrice = await bananaContract.methods.bananaPrice().call() 
+    setBananaPrice(bananaPrice)
+   
+  }
+  
+  async function mintWhale(how_many_whales) {
+    if (whaleContract) {
+ 
+      const price = Number(whalePrice)  * how_many_whales 
+
+      const gasAmount = await whaleContract.methods.mintBoringBanana(how_many_whales).estimateGas({from: walletAddress, value: price})
+      console.log("estimated gas",gasAmount)
+
+      console.log({from: walletAddress, value: price})
+
+      whaleContract.methods
+            .mintBoringBanana(how_many_whales)
+            .send({from: walletAddress, value: price, gas: String(gasAmount)})
+            .on('transactionHash', function(hash){
+              console.log("transactionHash", hash)
+            })
+          
+    } else {
+        console.log("Wallet not connected")
+    }
+    
+  };
+
   return (
     <div id="bodyy" className="flex flex-col items-center justify-center min-h-screen py-2">
-        <div id="about">
-          <div className="mt-6 border-b-2 py-6">
-            <div className="flex flex-wrap lg:flex-nowrap justify-around items-center">
-              <div className="lg:w-1/2 w-3/4">
-                <h1 className="text-7xl Poppitandfinchsans text-white ">EVERYONE, meet <br/><span className="text-blau">boring bananas</span></h1>
-                <p className="text-2xl text-white my-6  montserrat">Here at <span className="text-5xl Poppitandfinch ">boring</span> <span className="text-blau text-5xl Poppitandfinchsans">Bananas</span> <span className="text-5xl Poppitandfinch">company</span>, we specialize in the world's <span className="text-span-2 font-bold">finest digital bananas</span>.<br />‍<br />We&#x27;ve put together a team spanning 3 continents, to bring you some of the most <br/>‍<span className="text-blau text-5xl Poppitandfinchsans">NUTRITIOUS </span><span className="text-5xl Poppitandfinch ">and</span><span className="text-blau text-5xl Poppitandfinchsans"> DELICIOUS</span> <br/>bananas out there.
-                </p>
-                <p className="text-2xl text-white my-6  montserrat">
-                <strong>LAUNCH DATE:</strong> 29 June 2021. 2pm EST. <br />‍
-                <span className="text-white text-2xl montserrat"><strong>TOTAL SUPPLY: 8888</strong> Bananas.<br/><strong>PRICE: 0.025 ETH </strong>each.</span></p>
-              </div>
-              <img className="lg:w-1/2 w-3/4" src="img/whales.gif" width="500px" />
-            </div>
-          </div>
-        </div>    
-      </div>
+      <div className="md:w-2/3 w-4/5">
+        <div className="mt-6 border-b-2 py-6">
+          <div className="flex flex-col items-center">
+              <span className="flex Poppitandfinchsans text-5xl text-white items-center bg-grey-lighter rounded rounded-r-none my-4 ">TOTAL WHALES MINTED:  <span className="text-blau text-6xl"> {<>{totalSupply}</> } / 500</span></span>
+              
+          </div> 
+        </div>
+      </div>  
+    </div>  
     );
 };
